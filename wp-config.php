c450686a5ace43a9d4b2f94d5c48abd119c82277
gwapocom <gwapocom@github.com>
2026-08-12 13:13:40 +0800

    fix: db host wasmer deployment

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,8 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+// define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', getenv('DB_HOST') ?: '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
